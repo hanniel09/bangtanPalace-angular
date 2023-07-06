6e4301bb854667da071b3613f631e8e8f7db4012
hanniel09 <hannielvieira1227@gmail.com>
2023-07-06 16:13:02 -0300

    feature

--- bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
+++ bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
@@ -9,7 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatTableModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ]
 })
 export class AppMaterialModule { }
