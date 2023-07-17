d222b744e0688387204a8b47b9b628117241dcf4
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-17 17:07:29 -0300

    feature

--- bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
+++ bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
@@ -4,10 +4,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 
@@ -24,7 +25,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatMenuModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatInputModule
   ]
 })
 export class AppMaterialModule { }
