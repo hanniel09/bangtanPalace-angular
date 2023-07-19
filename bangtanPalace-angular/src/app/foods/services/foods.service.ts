618a1223bda139814be348811c0c2a9f370f48bb
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-18 22:48:21 -0300

    feature

--- bangtanPalace-angular/src/app/foods/services/foods.service.ts
+++ bangtanPalace-angular/src/app/foods/services/foods.service.ts
@@ -22,7 +22,7 @@ export class FoodsService {
   list(){
     return  [{ _id: '1',
     title: "Sushi",
-    image: 'https://classic.exame.com/wp-content/uploads/2017/05/sushi.jpg?quality=70&strip=info&w=1024',
+    image: 'https://bangtanpalacedatas.s3.sa-east-1.amazonaws.com/sushi.jpg',
     price: "39,99"
      }
    ];
