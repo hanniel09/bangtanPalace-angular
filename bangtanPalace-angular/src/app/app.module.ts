f9722f7878f79199ba23cce5b115ee1b3187b37c
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-24 14:27:43 -0300

    feature

--- bangtanPalace-angular/src/app/app.module.ts
+++ bangtanPalace-angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { AppComponent } from './app.component';
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
+    MatBadgeModule,
     HttpClientModule,
     ReactiveFormsModule
 
--- bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
+++ bangtanPalace-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,7 +27,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    MatInputModule
+    MatInputModule,
+    MatBadgeModule
   ]
 })
 export class AppMaterialModule { }
