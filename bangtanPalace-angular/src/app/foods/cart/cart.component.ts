044fc52bffe742710d266ec8e8e910e26ade36e9
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-27 09:31:21 -0300

    Cart update

--- bangtanPalace-angular/src/app/foods/cart/cart.component.ts
+++ bangtanPalace-angular/src/app/foods/cart/cart.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
 
+export class CartComponent {
+  cartItems = [
+    {
+      name: 'Sushi',
+      price: 39.99,
+      description: 'Amount: 1',
+      imageUrl: 'https://bangtanpalacedatas.s3.sa-east-1.amazonaws.com/sushi.jpg'
+    },
+    // Add more items as needed
+  ];
+
+  removeItemFromCart(item: any) {
+    // Implement the logic to remove the item from the cart
+    console.log('Removing item:', item);
+  }
 }
