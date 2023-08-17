d05d0a29fbbaed014b3937623bbf7d53965df09e
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-17 14:32:02 -0300

    feature

--- bangtanPalace-angular/src/app/foods/auth/auth-register/auth-register.component.ts
+++ bangtanPalace-angular/src/app/foods/auth/auth-register/auth-register.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-register',
   templateUrl: './auth-register.component.html',
   styleUrls: ['./auth-register.component.scss']
 })
-export class AuthRegisterComponent {
+export class AuthRegisterComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor( private formBuilder: FormBuilder) {}
+   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: "",
+      password: "",
+      isAdmin: "",
+    })
+
+   }
 }
